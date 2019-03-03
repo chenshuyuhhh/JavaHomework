42ba5a6510af956e6bc28869e550d8c9b9f03711
chenshuyuhhh <2468493317@qq.com>
2019-03-03 12:29:38 +0800

    push first homework about singleton pattern

---
+++ src/hw1of2/SingleClass.java
@@ -0,0 +1,16 @@
+package hw1of2;
+
+public class SingleClass {
+
+    // Hide the constructor of the class
+    private SingleClass() {
+    }
+
+    // instance of the class
+    private static SingleClass singleInstance = new SingleClass();
+
+    // public static operation that return the sole instance of the class
+    synchronized public static SingleClass getInstance(){
+        return singleInstance;
+    }
+}
---
+++ src/hw1of2/Test.java
@@ -0,0 +1,9 @@
+package hw1of2;
+
+public class Test {
+    public static void main(String[] args) {
+        SingleClass sc1 = SingleClass.getInstance();
+        SingleClass sc2 = SingleClass.getInstance();
+        System.out.println("sc1 和 sc2 是否是同一个实例？\n" + (sc1==sc2));
+    }
+}
